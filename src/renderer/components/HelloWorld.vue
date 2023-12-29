112d91e6a5279dd52e834b9cb72638cab126f01a
bailinsong <bailinsong@me.com>
2023-12-29 10:04:44 +0800

    添加 vuetify支持

---
+++ src/renderer/components/HelloWorld.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+defineProps({
+  msg: String,
+})
+
+const count = ref(0)
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+
+  <v-card
+      class="mx-auto"
+      width="400"
+
+  >
+    <v-btn type="button" @click="count++">count is {{ count }}</v-btn>
+    <p>
+      Edit
+      <code>components/HelloWorld.vue</code> to test HMR
+    </p>
+  </v-card>
+  <p>
+    Install
+    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
+    in your IDE for a better DX
+  </p>
+  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
